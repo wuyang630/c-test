2d3a1a71d1fa613383cd45d7f9de8a9982a6d9e3
wuyang630 <wuyang630@qq.com>
2014-04-28 14:47:59 +0800

    add ctest

---
+++ ctest/basic_server.cpp
@@ -0,0 +1,14 @@
+/*
+class BasicServer
+{
+	public:
+		BasicServer(){};
+		BasicServer(uint16_t _port):local_port(_port){};
+		server_init();
+		get_req();
+		handle_msg();
+		send_rsp();
+	private:
+		local_port;
+};
+*/
---
+++ ctest/basic_server.h
@@ -0,0 +1,13 @@
+
+class BasicServer
+{
+	public:
+		BasicServer(){};
+		BasicServer(uint16_t _port):local_port(_port){};
+		server_init();
+		get_req();
+		handle_msg();
+		send_rsp();
+	private:
+		local_port;
+};
